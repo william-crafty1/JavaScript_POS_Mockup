86898f61ba678fee252a086b348e6ac17ff43bdc
Gerard <gerard.dickerson1@gmail.com>
2020-06-18 21:54:47 -0400

    Slightly functioning store page with yellow background at this point

--- scripts/Gerard.js
+++ scripts/Gerard.js
@@ -74,5 +74,80 @@ class Payment {
   };
 };
 
-let payment = new Payment(100);
-payment.amountPaid(150)
+// let payment = new Payment(100);
+// payment.amountPaid(150)
+
+let removeCartItemButtons = document.getElementsByClassName("btn-danger")
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+  let button = removeCartItemButtons[i]
+  button.addEventListener("click", removeCartItem)
+  }
+
+  let quantityInputs = document.getElementsByClassName("cart-quantity-input")
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i]
+    input.addEventListener("changed", quantityChanged)
+  }
+
+  let addToCartButtons = document.getElementsByClassName("shop-button-item")
+  for (let i = 0; i < addToCartButtons.length; i++) {
+    let button = addToCartButtons[i]
+    button.addEventListener("clicked", addToCartClicked)
+  }
+
+function removeCartItem(event) {
+  let buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+  let input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1
+  }
+  updateCartTotal()
+}
+
+function addToCartClicked(event) {
+  let button = event.target
+  let shopItem = button.parentElement.parentElement
+  let title = shopItem.getElementsByClassName("shop-item-title")[0].innerText
+  let price = shopItem.getElementsByClassName("shop-item-price")[0].innerText
+  let imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src
+  addItemToCart(title, price, imageSrc)
+}
+
+function addItemToCart(title, price, imageSrc) {
+  let cartRow = document.createElement("div")
+  cartRow.classList.add("cart-row")
+  let cartItems = document.getElementsByClassName("cart-items")[0]
+  let cartRowContents = `
+      <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${imageSrc}" alt="" class="practice_image">
+        <span class="cart-item-title">Captain Planet</span>
+    </div>
+    <span class="cart-price cart-column">99.99</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" name="" id="" value="1">
+        <button class="btn btn-danger">REMOVE</button>
+    </div>`
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+}
+
+function updateCartTotal() {
+  let cartItemContainer = document.getElementsByClassName("cart-items")[0]
+  let cartRows = cartItemContainer.getElementsByClassName("cart-row")
+  let total = 0
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i]
+    let priceElement = cartRow.getElementsByClassName("cart-price")[0]
+    let quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+    let price = parseFloat(priceElement.innerText.replace("$", ""))
+    let quantity = quantityElement.value
+    total = total + (price * quantity)
+  }
+  total = Math.round(total * 100) / 100
+  document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total
+}
